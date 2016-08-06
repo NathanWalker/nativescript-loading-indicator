dbf6f70808a7dfb4a24a7aad287dec50b65c49fd
Nathan Walker <walkerrunpdx@gmail.com>
2016-08-05 17:58:03 -0700

    chore: fix typescript definition issue

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,19 +1,21 @@
 import {Observable} from "data/observable";
 import {LoadingIndicator} from "nativescript-loading-indicator";
 
+declare var MBProgressHUDModeDeterminate, MBProgressHUDModeAnnularDeterminate, MBProgressHUDModeDeterminateHorizontalBar, MBProgressHUDModeText, MBProgressHUDModeCustomView;
+
 export class LoadingTest extends Observable {
   private indicator: LoadingIndicator;
-  
+
   constructor() {
     super();
     this.indicator = new LoadingIndicator();
   }
-  
+
   public showLoader() {
     this.indicator.show({ message: 'Loading test...' });
     this.demoLoader();
   }
-  
+
   public showLoaderMsgAndDetails() {
     this.indicator.show({ message: 'One moment', ios: { details: 'Updating data...' } });
     setTimeout(() => {
@@ -21,58 +23,58 @@ export class LoadingTest extends Observable {
       this.demoLoader();
     }, 1000);
   }
-  
+
   public showLoaderSquare() {
     this.indicator.show({ ios: { square: true } });
     this.demoLoader();
   }
-  
+
   public showLoaderMargin() {
     this.indicator.show({ message: 'Message offset margin...', ios: { margin: 30 } });
     this.demoLoader();
   }
-  
+
   public showLoaderDimBg() {
     this.indicator.show({ ios: { dimBackground: true } });
     this.demoLoader();
   }
-  
+
   public showLoaderColor() {
     this.indicator.show({ ios: { color: '#4B9ED6' } });
     this.demoLoader();
   }
-  
+
   public showLoaderDeterminate() {
     this.indicator.show({ ios: { mode: MBProgressHUDModeDeterminate } });
     this.demoProgress();
   }
-  
+
   public showLoaderAnnularDeterminate() {
     this.indicator.show({ ios: { mode: MBProgressHUDModeAnnularDeterminate } });
     this.demoProgress();
   }
-  
+
   public showLoaderDeterminateHorizontalBar() {
     this.indicator.show({ ios: { mode: MBProgressHUDModeDeterminateHorizontalBar } });
     this.demoProgress();
   }
-  
+
   public showLoaderModeText() {
     this.indicator.show({ message: 'Text only', ios: { mode: MBProgressHUDModeText } });
     this.demoLoader();
   }
-  
+
   public showLoaderModeCustom() {
     this.indicator.show({ message: 'Completed', ios: { mode: MBProgressHUDModeCustomView, customView: 'Checkmark.png' } });
     this.demoLoader();
   }
-  
+
   private demoLoader() {
     setTimeout(() => {
       this.indicator.hide();
     }, 3000);
   }
-  
+
   private demoProgress() {
     setTimeout(() => {
       this.indicator.show({ progress: 0.15 });
