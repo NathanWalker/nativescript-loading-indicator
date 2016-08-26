ee9383dabcac57eb3533faad370d5d319fd74303
Nathan Walker <walkerrunpdx@gmail.com>
2016-08-25 21:05:26 -0700

    fix: android setMessage, also fixed TS issues
    closes
    https://github.com/NathanWalker/nativescript-loading-indicator/issues/9

--- src/android/progress-dialog.ts
+++ src/android/progress-dialog.ts
@@ -5,12 +5,12 @@ declare var android: any;
 
 export class LoadingIndicator {
   private _progressDialog: any;
-  
+
   public show(options?: OptionsCommon) {
     let context = this._getContext();
     if (context) {
-      if (options === undefined) options = {};
-      if (this._progressDialog === undefined) {
+      if (typeof options === 'undefined') options = {};
+      if (typeof this._progressDialog === 'undefined') {
         // Create
         let indeterminate = true;
         let cancelable = false;
@@ -18,11 +18,11 @@ export class LoadingIndicator {
           if (options.android.indeterminate !== undefined) indeterminate = options.android.indeterminate;
           if (options.android.cancelable !== undefined) cancelable = options.android.cancelable;
         }
-        
-        this._progressDialog = android.app.ProgressDialog.show(context, "", options.message || "Loading", indeterminate, cancelable);  
-      } else {
+
+        this._progressDialog = android.app.ProgressDialog.show(context, "", options.message || "Loading", indeterminate, cancelable);
+      } else if (this._progressDialog) {
         // options
-        if (options.message) this._progressDialog.setMesssage(options.message);
+        if (options.message && this._progressDialog.setMesssage) this._progressDialog.setMesssage(options.message);
         if (options.progress) this._progressDialog.setProgress(options.progress);
         // android specific
         if (options.android) {
@@ -31,18 +31,18 @@ export class LoadingIndicator {
           if (options.android.progressNumberFormat) this._progressDialog.setProgressNumberFormat(options.android.progressNumberFormat);
           if (options.android.progressPercentFormat) this._progressDialog.setProgressPercentFormat(options.android.progressPercentFormat);
           if (options.android.progressStyle) this._progressDialog.setProgressStyle(options.android.progressStyle);
-          if (options.android.secondaryProgress) this._progressDialog.setSecondaryProgress(options.android.secondaryProgress);  
+          if (options.android.secondaryProgress) this._progressDialog.setSecondaryProgress(options.android.secondaryProgress);
         }
       }
       return this._progressDialog;
     }
   }
-  
+
   public hide() {
     if (this._progressDialog !== undefined) this._progressDialog.hide();
     this._progressDialog = undefined;
   }
-  
+
   private _getContext() {
     return application.android.foregroundActivity;
   }
