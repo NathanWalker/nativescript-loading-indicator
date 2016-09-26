a82a571b883fac9d31a4d58f7c35a5ae50acea33
Nathan Walker <walkerrunpdx@gmail.com>
2016-09-26 09:01:56 -0700

    fix: iOS 10 compatibility changes
    closes
    https://github.com/NathanWalker/nativescript-loading-indicator/issues/14
    closes
    https://github.com/NathanWalker/nativescript-loading-indicator/pull/15

--- src/ios/mbprogress-hud.ts
+++ src/ios/mbprogress-hud.ts
@@ -1,5 +1,6 @@
 import {OptionsCommon} from '../interfaces';
 import {Color} from 'color';
+import * as utils from 'utils/utils';
 
 declare var MBProgressHUD: any;
 declare var MBProgressHUDModeCustomView: any;
@@ -9,15 +10,15 @@ declare var UIImage: any;
 
 export class LoadingIndicator {
   private _hud: any;
-  
+
   public show(options?: OptionsCommon) {
     if (this._hud === undefined) this._hud = MBProgressHUD.showHUDAddedToAnimated(this._getRootWindow(), true);
     if (options === undefined) options = {};
-    
+
     // options
     if (options.message) this._hud.labelText = options.message;
     if (options.progress) this._hud.progress = options.progress;
-    
+
     // ios specific
     if (options.ios) {
       if (options.ios.details) this._hud.detailsLabelText = options.ios.details;
@@ -27,7 +28,7 @@ export class LoadingIndicator {
       if (options.ios.color) {
         this._hud.color = new Color(options.ios.color).ios;
       }
-      
+
       if (options.ios.mode) {
         this._hud.mode = options.ios.mode;
         if (options.ios.mode === MBProgressHUDModeCustomView && options.ios.customView) {
@@ -36,15 +37,15 @@ export class LoadingIndicator {
       }
     }
 
-    return this._hud; 
+    return this._hud;
   }
-  
+
   public hide() {
     MBProgressHUD.hideHUDForViewAnimated(this._getRootWindow(), true);
     this._hud = undefined;
   }
-  
+
   private _getRootWindow() {
-    return UIApplication.sharedApplication().windows[0];
+    return utils.ios.getter(UIApplication, UIApplication.sharedApplication).windows[0];
   }
 }
